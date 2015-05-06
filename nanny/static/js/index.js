31cebb2a43e9eaf55926aeeddcab2e70173ea145
sai@trialx.com <sai@tialx.com>
2015-05-05 21:36:52 -0400

    one nanny

--- nanny/static/js/index.js
+++ nanny/static/js/index.js
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Geolocation</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      html, body, #map-canvas {
-        height: 100%;
-        margin: 0px;
-        padding: 0px
-      }
-    </style>
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBc8_PL8mgyBOazZpOuvC1MtceLrW5DigU"> </script>
-    <script>
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see a blank space instead of the map, this
 // is probably because you have denied permission for location sharing.
@@ -21,8 +6,7 @@ var map;
 
 function initialize() {
   var mapOptions = {
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 6
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
@@ -33,10 +17,10 @@ function initialize() {
       var pos = new google.maps.LatLng(position.coords.latitude,
                                        position.coords.longitude);
 
-      var marker = new google.maps.Marker({
+      var infowindow = new google.maps.InfoWindow({
         map: map,
         position: pos,
-        content: 'my location.'
+        content: 'Location found using HTML5.'
       });
 
       map.setCenter(pos);
@@ -67,10 +51,3 @@ function handleNoGeolocation(errorFlag) {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-    </script>
-  </head>
-  <body>
-    <div id="map-canvas"></div>
-  </body>
-</html>
